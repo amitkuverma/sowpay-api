9b1a2cf69abc5ee2cf781f3b856e6191c63ac87e
AMIT VERMA <amit.verma@softobotics.com>
2025-06-14 14:15:13 +0530

    Error resolved

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,26 +1,26 @@
-import { where } from 'sequelize';
-import AccountDetails from '../models/user/account.model';
+// import { where } from 'sequelize';
+// import AccountDetails from '../models/user/account.model';
 
-class AccountDetailsService {
-  async createAccountDetails(data: any) {
-    return AccountDetails.create(data);
-  }
+// class AccountDetailsService {
+//   async createAccountDetails(data: any) {
+//     return AccountDetails.create(data);
+//   }
 
-  async getAllAccountDetails() {
-    return AccountDetails.findAll();
-  }
+//   async getAllAccountDetails() {
+//     return AccountDetails.findAll();
+//   }
 
-  async getAccountDetailsById(id: number) {
-    return AccountDetails.findByPk(id);
-  }
+//   async getAccountDetailsById(id: number) {
+//     return AccountDetails.findByPk(id);
+//   }
 
-  async updateAccountDetails(id: number, data: any) {
-    return AccountDetails.update(data, { where: { accId: id } });
-  }
+//   async updateAccountDetails(id: number, data: any) {
+//     return AccountDetails.update(data, { where: { accId: id } });
+//   }
 
-  async deleteAccountDetails(id: number) {
-    return AccountDetails.destroy({ where: { accId: id } });
-  }
-}
+//   async deleteAccountDetails(id: number) {
+//     return AccountDetails.destroy({ where: { accId: id } });
+//   }
+// }
 
-export default new AccountDetailsService();
+// export default new AccountDetailsService();
